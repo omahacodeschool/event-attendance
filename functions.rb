3047dc8387738043c72da8a0ef4273114b3b6025
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-02-28 10:12:38 -0600

    created rb function to read in event list, started working on js side to make request and create html

---
+++ functions.rb
@@ -0,0 +1,8 @@
+def getEventInfoList()
+	eventList = []
+	CSV.foreach("events.csv", {headers: true, return_headers: false}) do |row|
+		eventList.push(row.to_hash)
+	end
+	jsonEventList = eventList.to_json
+	return jsonEventList
+end

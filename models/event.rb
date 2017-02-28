452c42817eaaf3b994678f6a59e6e9d0f324d1f8
Spencer Gerdes <skgerdes@yahoo.com>
2017-02-28 16:12:24 -0600

    Documented new functions

--- models/event.rb
+++ models/event.rb
@@ -8,6 +8,11 @@ class Event
     database.all("events")
   end
 
+  # Gets the event associated with the id
+  #
+  # id - String of a number.
+  #
+  # Returns a csv::row or an error hash.
   def eventById(id)
   	database = Database.new
   	database.getRowById("events", id)

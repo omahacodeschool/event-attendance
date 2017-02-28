76587c8bbe02ad0f8416d793fe0837a59ff7fdf5
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-02-28 10:38:32 -0600

    javascript completed, creates plain html to disply event info list

--- public/scripts.js
+++ public/scripts.js
@@ -1,34 +1,28 @@
-indow.addEventListener("load", function (){
+window.addEventListener("load", function (){
 
 	var ourRequest = new XMLHttpRequest();
 	ourRequest.open('GET', "/eventlist", true);
 	ourRequest.onload = function() {
 		var result = ourRequest.responseText;
 		onloadfunction(result);
-		}
-	}
+	};
 	ourRequest.send();
 
 	function onloadfunction(result) {
 		data = JSON.parse(result);
-		
-		createHTML(data);
-		
+		createHTML(data);		
 	}
 
 	function createHTML(data){
-		htmlToInsert = "<h1>People Information List</h1>";
+		htmlToInsert = "";
 		for (var i=0;i<data.length;i++) {
-			htmlToInsert += "<div><h3>Person #" + parseInt(i + 1) + "</h3>";
-			htmlToInsert += "<p>First name: " + data[i]["fname"] + "</p>";
-			htmlToInsert += "<p>Last name: " + data[i]["lname"] + "</p>";
-
-			htmlToInsert += "<p>Address: " + data[i]["address"] + "</p>";
-			htmlToInsert += "<p>           " + data[i]["city"] + ", " + data[i]["state"]
-
-			htmlToInsert += " " + data[i]["zip"] + "</p>";
-			htmlToInsert += "<p>Telephone number: " + data[i]["tel"] + "</p></div>";
+			time = data[i]['time'];
+			groupName = data[i]['group'];
+			eventName = data[i]['title'];
+			htmlToInsert += "<p>"+time;
+			htmlToInsert += " - " + groupName;
+			htmlToInsert += " <a href='#'>"+eventName+"</a></p>";
 		}
+		document.getElementsByTagName("body")[0].innerHTML = htmlToInsert;
 	}
-
 });

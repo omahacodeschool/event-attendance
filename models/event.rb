0a79523f471ce66d04e5ab2b6b2d66990222a7ac
Sumeet Jain <sumeet@sumeetjain.com>
2017-02-28 11:26:06 -0600

    Document methods

--- models/event.rb
+++ models/event.rb
@@ -1,4 +1,8 @@
 class Event
+
+  # Get all events.
+  # 
+  # Returns Array of Event Hashes.
   def all
     database = Database.new
     database.all("events")
--- services/database.rb
+++ services/database.rb
@@ -1,4 +1,10 @@
 class Database
+
+  # Get al rows from a table.
+  # 
+  # table - Table name String.
+  # 
+  # Returns Array of row Hashes.
   def all(table)
     list = []
     CSV.foreach("#{table}.csv", {headers: true, return_headers: false}) do |row|

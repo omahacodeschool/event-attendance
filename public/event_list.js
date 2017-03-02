2ceb3e2b4cea17858d6fd2e54ba26c8045dd8042
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-03-02 14:38:21 -0600

    in the middle of refactoring js into OO

--- public/event_list.js
+++ public/event_list.js
@@ -1,27 +1,100 @@
-function EventList(eventsByWeekday) {
-  this.eventsByWeekday = eventsByWeekday;
-  // @eventsByWeekday = eventsByWeekday
+function EventList(date = null) {
+  // this.eventsByWeekday = eventsByWeekday;
+  
+  // date is the date of the monday of the week as yyyy-mm-dd
+  this.date = date;
+
+  // monday is the date of the monday as a date object
+  this.monday = new Date();
+  this.mondayISO = this.monday.toISOString().substr(0,10);
 
   this.weekdays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+  this.months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
 
   this.htmlToInsert = "";
 
-  // Creates html for events for the week and add to index.erb.
-  // events_by_weekday - json data as a hash organized as weekday -> array of events.
-  // html added to index.erb.
-  this.createHTML = function(){
-    this.createHTMLForEachWeekday();
-
-    document.getElementsByClassName("events")[0].insertAdjacentHTML('beforeend',this.htmlToInsert);
+  this.addEvents = function() {
+    setMondayDate();
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', "/eventlist?date=" + this.date, true);
+    ourRequest.onload = function(){
+      this.eventsByWeekday = JSON.parse(event.target.responseText);
+      this.createHTML();
+    };
+    ourRequest.send();
   }
 
+  this.setMondayDate = function() {
+    if (this.date) {
+      this.monday.setTime(Date.parse(this.date.replace(/-/g, '\/')));
+    } else {
+      this.monday = this.getCurrentDate();
+      this.date = this.monday.toISOString().substr(0,10);
+    }
+  }
+
+  // get the date of what is currently being displayed
+  //
+  // returns a date of the format yyyy-mm-dd
+  function getDisplayedWeekDate() {
+    var dateHeader = document.getElementsByClassName("events-date")[0].firstElementChild;
+    var date = dateHeader.textContent.split("-");
+    var d = new Date(date[0] + date[1].substr(-4));
+    return d;
+  }
+
+
+
+
+    // will display the date range for the week as a header
+    // date is the date of the monday of the week as yyyy-mm-dd
+    // returns nothing, inserts header text in html h2
+  this.addHeader = function() {
+    setMondayDate();
+    dateString = this.createDateString();
+    document.getElementsByClassName("events-date")[0].firstElementChild.textContent = dateString;
+  }
+
+
+
+
+  // creates the string to be displayed in the header
+  // monday is a date object of a monday
+  // returns a string of the form "February 27 - March 5, 2017"
+  this.createDateString = function() { 
+    m1d1 = this.months[this.monday.getMonth()] + " " + this.monday.getDate();
+    var sunday = this.monday;
+    sunday.setDate(sunday.getDate() + 6);
+    m2d2 = this.months[sunday.getMonth()] + " " + sunday.getDate();
+    return m1d1 + " - " + m2d2 + ", " + this.monday.getFullYear();
+  }
+
+
+
+  // Creates html for events for the week and add to index.erb.
+  //
+  // events_by_weekday - json data as a hash organized as weekday -> array of events.
+  //
+  // Returns nothing, results in html added to index.erb.
+  this.createHTML = function(){
+    this.createHTMLForEachWeekday();
+    document.getElementsByClassName("events-list")[0].innerHTML = this.htmlToInsert;
+  }
+
+  // Gets a single day of events
+  //
+  // dayIndex - Integer from 0-6
+  //
+  // Returns an Array of events for one day
   this.singleDaysEvents = function(dayIndex){
     return this.eventsByWeekday[this.weekdays[dayIndex]];
   }
 
   // Creates html for events by each weekday.
+  //
   // data - json data as a hash organized as weekday -> array of events.
-  // html created as a string for all weekdays.
+  //
+  // Returns nothing, results in html created as a string for all weekdays.
   this.createHTMLForEachWeekday = function () {
     for (var i=0; i < this.weekdays.length; i++) {
       if (this.eventsByWeekday[this.weekdays[i]]){
@@ -34,7 +107,9 @@ function EventList(eventsByWeekday) {
   }
 
   // Creates html for events for a single weekday.
+  //
   // daysEvents - an array of events for a single weekday.
+  // 
   // html created as a string for one weekday.
   this.createHTMLForEachEvent = function (daysEvents) {
     for (var j=0; j<daysEvents.length;j++) {
--- public/scripts.js
+++ public/scripts.js
@@ -13,63 +13,17 @@ window.addEventListener("load", function (){
 	};
 
 	if (bodyHasClass("index_page")){
-		displayHeaderOnHomepage();
-		displayEventsOnHomepage();
+
+		displayHomepage();
 
 		// get events for a specific week from server 
 		// and display them by creating and inserting html
 		// on to homepage as a list
 		// date is the date of the monday of the week as yyyy-mm-dd
-		function displayEventsOnHomepage(date = null) {
-			var ourRequest = new XMLHttpRequest();
-			if (date) {
-				ourRequest.open('GET', "/eventlist?date=" + date, true);
-			} else {
-				ourRequest.open('GET', "/eventlist", true);
-			}
-			ourRequest.onload = addEvents;
-			ourRequest.send();
-		}
-
-		// will display the date range for the week as a header
-		// date is the date of the monday of the week as yyyy-mm-dd
-		// returns nothing, inserts header text in html h2
-		function displayHeaderOnHomepage(date=null) {
-			if (date) {
-				var monday = new Date();
-				monday.setTime(Date.parse(date.replace(/-/g, '\/')));
-			} else {
-				monday = getCurrentDate();
-			}
-			dateString = createDateString(monday);
-			document.getElementsByClassName("events-date")[0].firstElementChild.textContent = dateString;
-		}
-
-		// creates the string to be displayed in the header
-		// monday is a date object of a monday
-		// returns a string of the form "February 27 - March 5, 2017"
-		function createDateString(monday) {
-			var month = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-			m1d1 = month[monday.getMonth()] + " " + monday.getDate();
-			var sunday = monday;
-			sunday.setDate(sunday.getDate() + 6);
-			m2d2 = month[sunday.getMonth()] + " " + sunday.getDate();
-			return m1d1 + " - " + m2d2 + ", " + monday.getFullYear();
-		}
-
-		var ourRequest = new XMLHttpRequest();
-		ourRequest.open('GET', "/eventlist", true);
-		ourRequest.onload = addEvents;
-		ourRequest.send();
-
-		// Add all events to the homepage.
-		// event - get request for events.
-		// Adds the html to index for list of the week of events.
-		function addEvents(event) {
-			var data = JSON.parse(event.target.responseText);
-
-			var list = new EventList(data)
-			list.createHTML();
+		function displayHomepage(date) {
+			var list = new EventList(date)
+			list.addEvents();
+			list.addHeader();	
 		};
 
 		// create event listeners for previous and next buttons
@@ -89,8 +43,7 @@ window.addEventListener("load", function (){
 			var prevMonday = currentMonday;
 			prevMonday.setDate(currentMonday.getDate() - 7);
 			mondayDate = prevMonday.toISOString().substr(0,10);
-			displayHeaderOnHomepage(mondayDate);
-			displayEventsOnHomepage(mondayDate);
+			displayHomepage(mondayDate);
 		}
 
 		// run when next week button is clicked
@@ -103,19 +56,10 @@ window.addEventListener("load", function (){
 			var nextMonday = currentMonday;
 			nextMonday.setDate(currentMonday.getDate() + 7);
 			mondayDate = nextMonday.toISOString().substr(0,10);
-			displayHeaderOnHomepage(mondayDate);
-			displayEventsOnHomepage(mondayDate);
+			displayHomepage(mondayDate);
 		}
 
-		// get the date of what is currently being displayed
-		//
-		// returns a date of the format yyyy-mm-dd
-		function getCurrentDate() {
-			var dateHeader = document.getElementsByClassName("events-date")[0].firstElementChild;
-			var date = dateHeader.textContent.split("-");
-			var d = new Date(date[0] + date[1].substr(-4));
-			return d;
-		}
+
 
 	};
 

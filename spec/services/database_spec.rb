4185056831dd06700f5198c1c0ab4282c300787c
Spencer Gerdes <skgerdes@yahoo.com>
2017-03-02 17:54:57 -0600

    Changed test to check for length instead of class

--- spec/services/database_spec.rb
+++ spec/services/database_spec.rb
@@ -1,9 +1,11 @@
 RSpec.describe(Database, '.all') do
 	it 'gets all rows from a table' do
-		database = Database.new 
-		list = database.all("events")
-		# Refactor to expect list to equal the csv's length
-		expect(list.class).to eq(Array)
+		database = Database.new
+	
+		list = database.all("spec/fakeEvents")
+			binding.pry
+			# Maybe second test expecting list.class == Array?
+		expect(list.length).to eq(5)
 	end
 
 end

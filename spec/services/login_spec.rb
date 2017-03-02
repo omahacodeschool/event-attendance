b0b4f511ea68e9cb533ee9cbd5419f83e9625ef4
Sumeet Jain <sumeet@sumeetjain.com>
2017-03-02 09:16:09 -0600

    Add some tests to start things out.

---
+++ spec/services/login_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe(Login, '.valid') do
+  it "validates correct username and password" do
+    login_attempt = Login.valid("admin", "password")
+
+    expect(login_attempt).to eq(true)
+  end
+
+  it "validates incorrect username and password" do
+    login_attempt = Login.valid("admin", "wrong")
+
+    expect(login_attempt).to eq(false)
+  end
+end

8455706093ece46fda5954ca5308b02e7dbcb634
Spencer Gerdes <skgerdes@yahoo.com>
2017-03-02 15:25:34 -0600

    Deleted old vestigial funciton

--- services/database.rb
+++ services/database.rb
@@ -31,20 +31,6 @@ class Database
     return filtered_rows
   end
 
-
-  # Gets a single row from a table with an id.
-  #
-  # table - Table name string.
-  #
-  # Returns a csv::row or an error hash.
-  def getRowById(table, id)
-    CSV.foreach("#{table}.csv", {headers: true, return_headers:false}) do |row|
-      if row["id"] == id
-        return row.to_h
-      end
-    end
-  end
-
   # Adds a new row to the database
   #
   # array - an array containing three strings

e636ea51ae40c82f68b61099d6abec6ee1103234
Sumeet Jain <sumeet@sumeetjain.com>
2017-03-03 11:52:53 -0600

    Pollute specs with Sumeet's notes. TODO - Clean up his mess.

--- spec/services/database_spec.rb
+++ spec/services/database_spec.rb
@@ -1,10 +1,15 @@
 RSpec.describe(Database, '#all') do
 	
-	it 'gets all rows from a table' do
+	it 'returns 0 for an empty table' do
 		list = $database.all("events")
 
 		expect(list.length).to eq(5)
 	end
+
+  it 'returns correct value for a non-empty table' do
+    # Setup
+      # Delete all rows.
+  end
 
 	it 'returns an array of hashes from a table' do
 		list = $database.all("events")

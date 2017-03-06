96c35dfd3a76f630833b33a116f9b5ac7d0a0bf0
Sumeet Jain <sumeet@sumeetjain.com>
2017-03-06 16:12:27 -0600

    Indentation and making test case more robust

--- spec/services/database_spec.rb
+++ spec/services/database_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe(Database, "#all_with_filter") do
-	it 'returns a value from database' do
+	it 'gets a specified row' do
 		array = [["header1","header2"],["1","test 1"],["2","test 2"]]
 		CSV.open($database.table_path("comments"), "wb") do |csv|
 			array.each do |row|
@@ -8,12 +8,29 @@ RSpec.describe(Database, "#all_with_filter") do
    		end
 
    		filter = Proc.new do |row|
-     	row["header1"] == "1"
+     		row["header1"] == "1"
    		end
 
 		list = $database.all_with_filter("comments", filter)
-		expect(list.length).to eq(1)
+		expect(list).to include("1", "test 1")
 	end 
+
+	# TODO Add in this test, or add the assertion to the previous test
+	# it 'only gets the specified number of rows' do
+	# 	array = [["header1","header2"],["1","test 1"],["2","test 2"]]
+	# 	CSV.open($database.table_path("comments"), "wb") do |csv|
+	# 		array.each do |row|
+ #     		csv << row
+ #     		end
+ #   		end
+
+ #   		filter = Proc.new do |row|
+ #     		row["header1"] == "1"
+ #   		end
+
+	# 	list = $database.all_with_filter("comments", filter)
+	# 	expect(list.length).to eq(1)
+	# end 
 
 end 
 

4595a4dc41be07f23f1642be13c416e47b805ae9
Spencer Gerdes <skgerdes@yahoo.com>
2017-03-06 17:56:54 -0600

    Required new models

--- main.rb
+++ main.rb
@@ -9,6 +9,8 @@ require 'time'
 require_relative "services/login.rb"
 require_relative "models/event.rb"
 require_relative "models/user.rb"
+require_relative "models/rsvp.rb"
+require_relative "models/comment.rb"
 
 require_relative "controller.rb"
 require_relative "services/database.rb"

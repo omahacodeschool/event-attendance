44fdfa989c7df5cc7f98e92743542d22f0169d4f
Spencer Gerdes <skgerdes@yahoo.com>
2017-03-07 21:04:28 -0600

    Made function more coherent

--- models/user.rb
+++ models/user.rb
@@ -6,7 +6,7 @@ class User
   # 
   # Returns user info as a Hash.
   def User.create(email, pass, fullname, admin="false")
-    if !$database.checkifUniq(email, "users", "username")
+    if $database.checkifUniq(email, "users", "username")
       userInfo = [email, pass, fullname, admin]
       $database.newRow(userInfo, "users")
 

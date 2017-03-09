fafb544ecb5d285f46c6c14e19c5bca3ecf65785
Spencer Gerdes <skgerdes@yahoo.com>
2017-03-08 19:35:05 -0600

    Documented method and added note about condensing functions

--- spec/support/database_helper.rb
+++ spec/support/database_helper.rb
@@ -1,17 +1,19 @@
 class DatabaseHelper
 
+  # Empties everything from a table
+  #
+  # table - table's name as a String
   def DatabaseHelper.empty(table)
     $sql.exec( "DELETE FROM #{table}" )
   end
 
-  # Writes rows to a table
+  # Writes rows to a table --!important( this replaces addRows )
   #
-  # table - name of file
-  # valuesString - string like "(value1,value2),(value3,value4)"
+  # table        - name of file as String
+  # valuesString - String like "(value1,value2),(value3,value4)"
   def DatabaseHelper.writeTable(valuesString,table)
       $sql.exec("INSERT INTO #{table} VALUES #{valuesString}")
   end
-
 
   # Writes rows to a table
   #
@@ -23,7 +25,6 @@ class DatabaseHelper
       $sql.exec( "INSERT INTO #{table} VALUES (#{chunkOfContent})")
     end
   end
-
 
   def DatabaseHelper.add_row_with_attributes(table, attributes, content)
     content.each do |each|

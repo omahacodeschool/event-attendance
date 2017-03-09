213afc7a629d4f05017041a1e1cc186140434c0a
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-03-09 15:34:35 -0600

    tests working for add and delete rsvp

--- spec/models/rsvp_spec.rb
+++ spec/models/rsvp_spec.rb
@@ -4,30 +4,70 @@ RSpec.describe(RSVP, '.for_event') do
 
 end
 
-RSpec.describe(RSVP, '.delete') do
 
-	pending
+RSpec.describe(RSVP, ".delete") do
+	
+	it "deletes an attendee from an event" do
+		# Setup
+	  	DatabaseHelper.writeToTable("rsvps", "('9','Mickey Mouse')","(eventid,fullname)")
+
+		# Exercise
+		RSVP.delete('9','Mickey Mouse')
+		
+		# Verify
+		sqlNames = $sql.exec("SELECT fullname FROM rsvps").to_a
+		expect(sqlNames.to_s).not_to include('Mickey Mouse')
+
+		# Teardown
+		DatabaseHelper.empty("rsvps")
+	end
+
+	it "does not delete other attendees from an event" do
+		# Setup
+		values = "('9', 'Mickey Mouse'), ('9', 'Minnie Mouse')"
+	  	DatabaseHelper.writeToTable("rsvps", values, "(eventid,fullname)")
+
+		# Exercise
+		RSVP.delete('9', 'Mickey Mouse')
+		
+		# Verify
+		sqlNames = $sql.exec("SELECT fullname FROM rsvps").to_a
+		expect(sqlNames.to_s).to include('Minnie Mouse')
+
+		# Teardown
+		DatabaseHelper.empty("rsvps")
+	end
+
+	it "does not delete rsvps to other events for the attendee" do
+		# Setup
+		values = "('9', 'Mickey Mouse'), ('10', 'Mickey Mouse')"
+	  	DatabaseHelper.writeToTable("rsvps", values, "(eventid,fullname)")
+
+		# Exercise
+		RSVP.delete('9', 'Mickey Mouse')
+		
+		# Verify
+		sqlNames = $sql.exec("SELECT eventid FROM rsvps").to_a
+		expect(sqlNames.to_s).to include('10')
+
+		# Teardown
+		DatabaseHelper.empty("rsvps")
+	end
 
 end
 
-RSpec.describe(RSVP, '.add') do
-
-	pending
-
-end
-
-RSpec.describe(Event, "#addAttendee") do
+RSpec.describe(RSVP, ".add") do
 	
 	it "adds a new attendee to an event" do
 		# Setup
-		event = Event.new("9")
+		eventId = "9"
 		newRsvp = "Dan Gheesling"
 
-		# Excercise
-		event.addAttendee(newRsvp)
-		sqlNames = $sql.exec("SELECT fullname FROM rsvps").to_a
-
+		# Exercise
+		RSVP.add(eventId,newRsvp)
+		
 		# Verify
+		sqlNames = $sql.exec("SELECT fullname FROM rsvps").to_a
 		expect(sqlNames.to_s).to include(newRsvp)
 
 		# Teardown
@@ -36,15 +76,15 @@ RSpec.describe(Event, "#addAttendee") do
 
 	it "stores the correct events id with the new rsvper" do
 		# Setup
-		event = Event.new("25")
-		newRsvp = "Bugs Bunny"
-		
-		# Excercise
-		event.addAttendee(newRsvp)
-		sqlResult = $sql.exec("SELECT eventid FROM rsvps WHERE fullname='#{newRsvp}'").to_a
+		eventId = "9"
+		newRsvp = "Dan Gheesling"
+
+		# Exercise
+		RSVP.add(eventId,newRsvp)
 		
 		# Verify
-		expect(sqlResult[0]['eventid']).to eq("25")
+		sqlResult = $sql.exec("SELECT * FROM rsvps WHERE fullname='#{newRsvp}'").to_a
+		expect(sqlResult[0]['eventid']).to eq(eventId)
 		
 		# Teardown
 		DatabaseHelper.empty('rsvps')

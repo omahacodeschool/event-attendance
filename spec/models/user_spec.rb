078d73896cff533ab58166036d6853a57bdec198
Spencer Gerdes <skgerdes@yahoo.com>
2017-03-09 11:40:29 -0600

    fixed indenting, sorry

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,13 @@ RSpec.describe( User, '.create') do
 		email = "foobar@foo.com"
 		pass = "banana"
 		fullname = "Mr. Kiwi"
+
 		# Excersize
 		newUser = User.create(email, pass, fullname)
+
 		# Verify
 		expect(newUser).to eq({"username"=>email,"fullname"=>fullname, "admin" => "false"})
+
 		# Teardown
 		DatabaseHelper.empty('users')
 	end
@@ -18,11 +21,14 @@ RSpec.describe( User, '.create') do
 		email = "bat@man.com"
 		pass = "Bruce"
 		fullname = "Bruce Wayne"
+
 		# Excercise
 		newUser = User.create(email, pass, fullname)
 		sqlFirstRow = $sql.exec("SELECT * FROM users")[0]
+
 		# Verify
 		expect(sqlFirstRow.values).to include(fullname && email)
+
 		# Teardown
 		DatabaseHelper.empty('users')
 	end
@@ -31,11 +37,14 @@ RSpec.describe( User, '.create') do
 		# Setup
 		firstUser = "('Test@nothing.edu', 'password123', 'Email Thief')"
 		DatabaseHelper.writeToTable('users', firstUser)
+		
 		# Excercise
 		newUser = User.create("Test@nothing.edu", "security", "Smart T.")
 		numberOfUsers = $sql.exec("SELECT * FROM users").to_a.length
+		
 		# Verify
 		expect(numberOfUsers).to eq(1)
+		
 		# Teardown
 		DatabaseHelper.empty('users')
 	end

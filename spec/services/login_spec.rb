510978662a8fcb40688fbdd99a18b5f68417106c
Spencer Gerdes <skgerdes@yahoo.com>
2017-03-08 19:09:46 -0600

    indentation

--- spec/services/login_spec.rb
+++ spec/services/login_spec.rb
@@ -1,28 +1,34 @@
 RSpec.describe(Login, '.valid') do
 
 	it "validates correct username and password" do
-  	#set-up
+	  	#set-up
 		table = "users"
 		dataFiller="('admin@gmail.com','password','admin','true')"
 		DatabaseHelper.writeTable(dataFiller,table)
-	#exercise
+
+		#exercise
     	login_attempt = Login.valid("admin@gmail.com", "password")
-	#verify
+
+		#verify
     	expect(login_attempt).to be_truthy
-	#teardown
+
+		#teardown
 		DatabaseHelper.empty(table)
 	end
 
   it "validates incorrect username and password" do
-    #set-up
+	    #set-up
 		table = "users"
 		dataFiller="('admin@gmail.com','password','admin','true')"
 		DatabaseHelper.writeTable(dataFiller,table)
-	#exercise
+
+		#exercise
     	login_attempt = Login.valid("admin@gmail.com", "wrong")
-	#verify
+
+		#verify
     	expect(login_attempt).to be_falsey
-	#teardown
+    	
+		#teardown
 		DatabaseHelper.empty(table)
   end
   

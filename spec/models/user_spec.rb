a2da6c5e9bcc5341a4256f597d8c588b900b7141
Spencer Gerdes <skgerdes@yahoo.com>
2017-03-08 19:16:35 -0600

    uncommented tests for User class

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,58 +1,52 @@
-# RSpec.describe( User, '.create') do 
+RSpec.describe( User, '.create') do 
 
-# 	it "returns a Hash with the newly created user's info" do 
+	it "returns a Hash with the newly created user's info" do 
+		# Setup
+		email = "foobar@foo.com"
+		pass = "banana"
+		fullname = "Mr. Kiwi"
 
-# 		# Setup
-# 		email = "foobar@foo.com"
-# 		pass = "banana"
-# 		fullname = "Mr. Kiwi"
+		# Excersize
+		newUser = User.create(email, pass, fullname)
 
-# 		# Excersize
-# 		newUser = User.create(email, pass, fullname)
+		# Verify
+		expect(newUser).to eq({"username"=>email,"fullname"=>fullname, "admin" => "false"})
 
-# 		# Verify
-# 		expect(newUser).to eq({"username"=>email,"fullname"=>fullname, "admin" => "false"})
+		# Teardown
+		DatabaseHelper.empty('users')
+	end
 
-# 		# Teardown
-# 		DatabaseHelper.empty('users')
+	it "adds the new user to the database" do
+		# Setup
+		email = "bat@man.com"
+		pass = "Bruce"
+		fullname = "Bruce Wayne"
 
-# 	end
+		# Excercise
+		newUser = User.create(email, pass, fullname)
+		sqlFirstRow = $sql.exec("SELECT * FROM users")[0]
 
-# 	it "adds the new user to the database" do
+		# Verify
+		expect(sqlFirstRow.values).to include(fullname && email)
 
-# 		# Setup
-# 		email = "bat@man.com"
-# 		pass = "Bruce"
-# 		fullname = "Bruce Wayne"
+		# Teardown
+		DatabaseHelper.empty('users')
+	end
 
-# 		# Excercise
-# 		newUser = User.create(email, pass, fullname)
-# 		sqlFirstRow = $sql.exec("SELECT * FROM users")[0]
+	it "doesn't add a user if the email is taken" do
+		# Setup
+		firstUser = ["'Test@nothing.edu', 'password123', 'Email Thief'"]
+		DatabaseHelper.addRows('users', firstUser)
 
-# 		# Verify
-# 		expect(sqlFirstRow.values).to include(fullname && email)
+		# Excercise
+		newUser = User.create("Test@nothing.edu", "security", "Smart T.")
+		numberOfUsers = $sql.exec("SELECT * FROM users").to_a.length
 
-# 		# Teardown
-# 		DatabaseHelper.empty('users')
+		# Verify
+		expect(numberOfUsers).to eq(1)
 
-# 	end
+		# Teardown
+		DatabaseHelper.empty('users')
+	end
 
-# 	it "doesn't add a user if the email is taken" do
-		
-# 		# Setup
-# 		firstUser = ["'Test@nothing.edu', 'password123', 'Email Thief'"]
-# 		DatabaseHelper.addRows('users', firstUser)
-
-# 		# Excercise
-# 		newUser = User.create("Test@nothing.edu", "security", "Smart T.")
-# 		numberOfUsers = $sql.exec("SELECT * FROM users").to_a.length
-
-# 		# Verify
-# 		expect(numberOfUsers).to eq(1)
-
-# 		# Teardown
-# 		DatabaseHelper.empty('users')
-
-# 	end
-
-# end
+end

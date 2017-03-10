9b4efa027c776ec9ac92c67f942e1e34e9bc40a2
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-03-10 15:21:26 -0600

    meetup functions commented

--- services/meetup.rb
+++ services/meetup.rb
@@ -27,8 +27,10 @@ class Meetup
     end
   end
 
-  # group is the 
+  # Accesses the meetup API and makes a Meetup object for each event 
   #
+  # group - the slug for the group meetup url
+  # 
   #
   def Meetup.events(group)
     response_as_array = JSON.parse(Meetup.response(group))
@@ -39,17 +41,27 @@ class Meetup
     end
   end
 
+  # Get events for a group from the Meetup api
+  #
+  # group - the slug for the group meetup url
+  # 
+  # returns the api response as a String
   def Meetup.response(group)
     uri = URI('https://api.meetup.com/' + group + '/events')
     Net::HTTP.get(uri)
   end
 
+  # Checks if an event is already in the database and delete it if so
+  #
+  # Returns nothing, this is necessary to be able to update event info
   def overwriteEntry
     if $database.checkExistenceOf("events", "id" , @id)
       $database.deleteRow("events", "id = '#{@id}'")
     end
   end
 
+
+  # adds event to the event table of the database
   def save_to_events
     Event.create({
       :id => @id,
@@ -64,25 +76,32 @@ class Meetup
     })
   end
 
+  # removes ' from names since they are difficult to insert in sql
+  # 
+  # returns group name
   def group_name
     name = @event_info["group"]["name"]
     name.gsub(/'/, "")
   end
 
-   def title
+  # returns event title
+  def title
     @event_info["name"]
   end
 
-   def date
+  # returns event date as yyyy-mm-dd
+  def date
     d = Time.at(@event_info["time"]/1000)
     date = d.strftime('%F')
   end
 
-   def time
+  # returns event time as hh:mm pm/am
+  def time
     d = Time.at(@event_info["time"]/1000)
     time = d.strftime('%I:%M %p')
   end
 
+  # returns event location or tbd if none provided
   def location
     if @event_info["venue"]
       return @event_info["venue"]["name"]
@@ -91,6 +110,7 @@ class Meetup
     end
   end
 
+  # returns event address or empty string if none provided
   def address
     if @event_info["venue"]
        return @event_info["venue"]["address_1"]
@@ -99,10 +119,12 @@ class Meetup
     end
   end
 
+  # returns full link url to event 
   def link
     @event_info["link"]
   end
 
+  # returns the first 2 sentences of the description
   def description
     cleanedDescription = cleanDescription
     return shorten(cleanedDescription)
@@ -111,12 +133,12 @@ class Meetup
 private
 
   def cleanDescription
-    # Replaces all html tags
     if @event_info['description']
+      # Removes all html tags
       clean = @event_info["description"].gsub(/<(.*?)>/, "")
-      # Replaces unicode
+      # Removes unicode
       cleaner = clean.gsub(/\\u\d{4}/, "")
-      # Replaces ' 
+      # Removes ' 
       cleanest = cleaner.gsub("'","") 
     else
       @event_info["description"] = "No description"

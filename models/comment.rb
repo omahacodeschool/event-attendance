b994ab254019b99178006b4aca310dcf2acf96e8
Spencer Gerdes <skgerdes@yahoo.com>
2017-03-10 13:33:24 -0600

    Removed rogue pry

--- models/comment.rb
+++ models/comment.rb
@@ -20,7 +20,6 @@ class Comment
   #   then sorts the comments based on Id so they appear in
   #   the same spot as before edit
   def Comment.edit(params, user_fullname)
-    binding.pry
     filter = "id = '#{params["commentId"]}' AND fullname = '#{user_fullname}'"
     $database.updateRow("comments","comment",params["textContent"],filter)
   end
